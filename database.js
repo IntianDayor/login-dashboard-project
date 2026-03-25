ec5116f6a0d69fd789f87d99b2a5e3209e9cd3a3
IntianDayor <dayorferaer@gmail.com>
2026-03-25 15:05:26 +0800

    signup feature might be the endpoint of not using a database/backend

---
+++ database.js
@@ -0,0 +1,9 @@
+// This is for testing purposes only.
+
+export const psuedoDatabase = {
+    users: [
+        { username: 'admin', password: 'password' },
+        { username: 'intian', password: '1234' },
+        { username: 'dayor', password: 'nataekaba' }
+    ]
+};
