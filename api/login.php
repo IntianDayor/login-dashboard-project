aa714ba0d269ac2b7fdee03d46b7f566de7ba359
IntianDayor <dayorferaer@gmail.com>
2026-04-20 10:52:18 +0800

    Update: Admin Panel

--- api/login.php
+++ api/login.php
@@ -14,11 +14,14 @@ $stmt->execute();
 
 $result = $stmt->get_result();
 
-// Check if user exists and verify password
+// Check if user exists and verify password and if the user is an admin or not.
 if ($result->num_rows > 0) {
     $user = $result->fetch_assoc();
     if (password_verify($password, $user['password'])) {
-        echo json_encode(["success" => true, "user" => $user]);
+        echo json_encode([
+            "success" => true, 
+            "isAdmin" => $user['role'] === 'admin',
+            "user" => $user]);
     } else {
         echo json_encode(["success" => false, "message" => "Invalid password"]);
     }
