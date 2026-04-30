f7acc46bb5203616cd9b0e36009ad1e4a2153044
IntianDayor <dayorferaer@gmail.com>
2026-04-30 12:52:07 +0800

    Improved Backend. Added .env

--- api/db.php
+++ api/db.php
@@ -1,17 +1,19 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "password";
-$db   = "fprojectdb_mysql";
 
-$conn = new mysqli($host, $user, $pass, $db);
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Check connection
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$name = $_ENV['DB_NAME'];
+
+$conn = new mysqli($host, $user, $pass, $name);
+
 if ($conn->connect_error) {
-    echo json_encode([
-        "success" => false,
-        "message" => "Database connection failed"
-    ]);
-    exit;
+    die("Database connection failed: " . $conn->connect_error);
 }
+
 ?>
