d142ae4bbe46a930b3330e5c8aad3f177aea0770
IntianDayor <dayorferaer@gmail.com>
2026-07-20 11:00:29 +0800

    Removed 'UserView' for projects preview

--- assets/scripts/manage-proj-script.js
+++ assets/scripts/manage-proj-script.js
@@ -9,23 +9,6 @@ function esc(str) {
         .replace(/'/g, "&#39;");
 }
 
-// Script for managing project view and user preview navigation //
-
-const showUserViewButton = document.getElementById('user-view');
-const backToManageProjectsButton = document.getElementById('back-to-manage-projects');
-
-if (showUserViewButton) {
-    showUserViewButton.addEventListener('click', () => {
-        window.location.href = 'proj-user-preview.html';
-    });
-}
-
-if (backToManageProjectsButton) {
-    backToManageProjectsButton.addEventListener('click', () => {
-        window.location.href = 'projects.html';
-    });
-}
-
 // Upload/Delete Projects Script //
 
 const addProjectbtn = document.getElementById('add-project');
